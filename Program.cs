99724e057eb7173b56701b632bbf5b45c0744206
Luis Guijarro Pérez <luisgp82@hotmail.es>
2019-10-22 20:19:58 +0200

    Add arguments to .exe

--- Program.cs
+++ Program.cs
@@ -22,10 +22,44 @@ namespace VulkanParser
 		[STAThread]
 		private static void Main(string[] args)
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm());
-		}
+            if (args.Length <= 0)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            else
+            {
+                for (int i=0;i<args.Length;i++)
+                {
+                    switch(args[i])
+                    {
+                        case "-n":
+                            string nspace = args[i + 1];
+                            i++;
+                            Console.WriteLine("Namespace: " + nspace);
+                            break;
+                        case "-c":
+                            string dllname = args[i + 1];
+                            i++;
+                            Console.WriteLine("Dll name: " + dllname);
+                            break;
+                        case "-b":
+                            //string dllname = args[i + 1];
+                            break;
+                        case "-h":
+                            Console.WriteLine("Vulkan Parser for C# developed by The BlackSword Brotherhood.");
+                            Console.WriteLine("Help info.");
+                            Console.WriteLine();
+                            Console.WriteLine("-n <namespace>  : Definir espacio de nombres del proyecto.");
+                            Console.WriteLine("-c <nombre.dll> : Compilar .cs en librería con nombre determinado.");
+                            Console.WriteLine("-b              : Generar clases extra (VKDevice) y sus dependencias.");
+                            Console.WriteLine("-h              : Muestra esta ayuda.");
+                            break;
+                    }
+                }
+            }
+        }
 		
 	}
 }
