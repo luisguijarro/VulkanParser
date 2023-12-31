b6ccf31086c1a3350d2a0f9f61407c56ba92ccae
Luis Guijarro Pérez <luisgp82@gmail.com>
2023-12-31 16:28:24 +0100

    Adding initial Type Parser

---
+++ Parser/TypesParser.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Xml;
+using System.Collections.Generic;
+
+namespace VulkanParser
+{
+    public static partial class Parser
+    {
+        public static Dictionary<string, string> BaseTypesList; // name, type
+        public static void ParseBaseTypes(XmlDocument xdoc, bool verbose)
+        {
+            BaseTypesList = new Dictionary<string, string>();
+            XmlNodeList? xml_baseTypes = xdoc.SelectNodes("/registry/types/type[@category='basetype']");
+            if (xml_baseTypes != null)
+            {
+                for (int i = 0; i < xml_baseTypes.Count; i++)
+                {
+                    if (xml_baseTypes[i] != null)
+                    {
+                        XmlNode? nameNode = xml_baseTypes[i]?.SelectSingleNode("name");
+                        XmlNode? cTypeNode = xml_baseTypes[i]?.SelectSingleNode("type");
+
+                        if (nameNode != null && cTypeNode != null)
+                        {
+                            string typeName = nameNode.InnerText;
+                            string typeType = TypeConversor.GetCSharpType(cTypeNode.InnerText);
+
+                            BaseTypesList.Add(typeName, typeType);
+
+                            if (verbose) { Console.WriteLine($"Getted Type {typeName}, {typeType}"); }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
