23e7eab5d6cdaf0fba8d6d7bd12aa9afdbcd93d3
Luis Guijarro Pérez <luisgp82@gmail.com>
2024-01-23 22:25:57 +0100

    Adding Constants, enums, bitmask, and commands parsers calls.

--- vkParser.cs
+++ vkParser.cs
@@ -1,17 +1,20 @@
 
 using System.Xml;
 
-namespace VulkanParser
+namespace VulkanParser;
+
+public static partial class Parser
 {
-    public static partial class Parser
+    public static void Parse(string xmlFilePath, bool verbose)
     {
-        public static void Parse(string xmlFilePath, bool verbose)
-        {
-            XmlDocument xdoc = new XmlDocument();
-            xdoc.Load(xmlFilePath);
-            ParseBaseTypes(xdoc, verbose);
-            ParseBitmaskTypes(xdoc, verbose);
-            ParseDefineTypes(xdoc, verbose);
-        }
+        XmlDocument xdoc = new XmlDocument();
+        xdoc.Load(xmlFilePath);
+        ParseBaseTypes(xdoc, verbose);
+        ParseBitmaskTypes(xdoc, verbose);
+        ParseDefineTypes(xdoc, verbose);
+        ParseConstants(xdoc, verbose);
+        ParseEnums(xdoc, verbose);
+        ParseBitmasks(xdoc, verbose);
+        ParseCommands(xdoc, verbose);
     }
 }
