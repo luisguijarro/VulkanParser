c7e0bdbca94b00ef4eaa7c405ffd63f6a92b111a
Luis Guijarro Pérez <luisgp82@gmail.com>
2024-01-23 22:22:05 +0100

    Adding one utility to convers some values.

---
+++ utils/ValuesConversor.cs
@@ -0,0 +1,10 @@
+namespace VulkanParser
+{
+    public static class ValuesConversor
+    {
+        public static string FromCValue(string cValue)
+        {
+            return cValue.Replace("(", "").Replace(")", "").Replace("ULL", "ul").Replace("U", "u");
+        }
+    }
+}
